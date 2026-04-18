762bc7d5fc6aa34968ded319d4e453ad1e48634c
noah202055-eng <noah202055@gmail.com>
2026-04-18 16:24:24 -0400

    Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
-public class Sudoku{
-    private int[][] sudo;
+import java.util.Collections;
+public class Sudoku {
+    private int[][]sudo;
     private ArrayList<Integer> list;
     public static void main(String[] args) {
         Sudoku sudok = new Sudoku();
@@ -16,20 +17,20 @@ public class Sudoku{
             int r = (int) (Math.random() * 9);
             int temp = list.get(i);
             list.set(i, list.get(r));
-            list.set(r, temp);
+            list.set(r,temp);
         }
         fillBoard();
     }
     public void fillBoard() {
-        solve(0, 0);
+        pathfinder(0, 0); 
     }
     public void printBoard() {
         System.out.println("+-------+-------+-------+");
-        for (int r =0;r < 9; r++) {
+        for (int r = 0; r < 9; r++) {
             System.out.print("| ");
             for (int c = 0; c < 9; c++) {
                 System.out.print(sudo[r][c] + " ");
-                if ((c + 1) % 3 == 0) {
+                if ((c + 1)% 3== 0) {
                     System.out.print("| ");
                 }
             }
@@ -39,48 +40,58 @@ public class Sudoku{
             }
         }
     }
-    public boolean solve(int row, int col) {
-        if (row == 9){
+    public boolean pathfinder(int r,int c) {
+        if (c == 9){
             return true;
         }
-        if (col == 9){
-            return solve(row + 1,0);
+        if (r == 9) {
+            return pathfinder(0, c + 1);
         }
-        if (sudo[row][col] != 0){
-            return solve(row, col+ 1);
+
+        if (sudo[r][c] != 0) {
+            return pathfinder(r + 1, c);
         }
-        for (int k = 0; k < 9; k++) {
-            int num = list.get(k);
-            if (isSafe(row, col, num)) {
-                sudo[row][col] =num;
-                if (solve(row, col + 1)){
+        ArrayList<Integer> values = new ArrayList<>(); 
+        for (int i = 1; i <= 9; i++) {
+            values.add(i);
+        }
+        Collections.shuffle(values);
+        int index = 0;
+        while (index < 9) {
+            int val= values.get(index);
+
+            if (isSafe(r,c, val)) {
+                sudo[r][c] = val;
+                if (pathfinder(r + 1, c)) {
                     return true;
                 }
-                sudo[row][col] =0;
+                sudo[r][c] = 0;
             }
+            index++;
         }
         return false;
     }
     public boolean isSafe(int row, int col, int num) {
         for (int i = 0; i < 9; i++) {
-            if (sudo[row][i] == num){
+            if (sudo[row][i] == num) {
                 return false;
             }
         }
         for (int i = 0; i < 9; i++) {
-            if (sudo[i][col] == num){
+            if (sudo[i][col] == num) {
                 return false;
             }
         }
-        int boxRow = (row / 3) *3;
+        int boxRow = (row / 3) * 3;
         int boxCol = (col / 3) * 3;
         for (int r = boxRow; r < boxRow + 3; r++) {
             for (int c = boxCol; c < boxCol + 3; c++) {
-                if (sudo[r][c] == num){
+                if (sudo[r][c] == num) {
                     return false;
                 }
             }
         }
+
         return true;
     }
 }
